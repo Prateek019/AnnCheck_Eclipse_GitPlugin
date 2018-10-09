2f2aebb5a22927c0e21685aa858c289fe47cfe2a
Prateek019 <prateeksinha15@gmail.com>
2018-10-09 15:54:25 +0530

    # WARNING: head commit changed in the meantime
    Some Sample commits from eclipse git plugin

--- AnnCheck/src/AnnCheck.java
+++ AnnCheck/src/AnnCheck.java
@@ -3,7 +3,7 @@ import java.lang.annotation.RetentionPolicy;
 
 //creates an annotation
 
-/*
+/*ihafluaheflsf
  * @Retention(RetentionPolicy.SOURCE)
  * @Retention(RetentionPolicy.CLASS) ->Compiler
  */
